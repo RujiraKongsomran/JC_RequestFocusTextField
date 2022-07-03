d1f632db3761f2345d5410872870e732f5724117
RujiraKongsomran <rujirabiwberry@gmail.com>
2022-07-03 21:22:05 +0700

    Request Focus

--- app/src/main/java/com/rujirakongsomran/jc_requestfocustextfield/MainScreen.kt
+++ app/src/main/java/com/rujirakongsomran/jc_requestfocustextfield/MainScreen.kt
@@ -1,21 +1,25 @@
 package com.rujirakongsomran.jc_requestfocustextfield
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rujirakongsomran.jc_requestfocustextfield.ui.theme.JC_RequestFocusTextFieldTheme
 
 @Composable
 fun MainScreen() {
+    val focusRequester = remember { FocusRequester() }
     var value by remember { mutableStateOf("") }
+
+    LaunchedEffect(key1 = Unit) {
+        focusRequester.requestFocus()
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -24,6 +28,7 @@ fun MainScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         TextField(
+            modifier = Modifier.focusRequester(focusRequester),
             value = value,
             onValueChange = { value = it },
             placeholder = {
